7eba55fd88b285d6377f5454880c84202ed56d36
Ulises <dr.manzanas1@gmail.com>
2020-07-28 17:38:18 +0200

    dockerfile

--- nodeBack/app.js
+++ nodeBack/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 const mysql = require('mysql');
 const cors = require('cors')
-const port = 2424
+const port = process.env.PORT || 2424;
 const bodyParser = require("body-parser")
 
 
